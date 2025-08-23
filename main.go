01a9a157250148a4d8bd3c311434d1897355e094
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2025-08-23 16:03:46 +0700

    add package air for live reload

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"crud-go/config"
+	"crud-go/controllers/homecontroller"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.ConnectDB()
+
+	// 1. Home
+	http.HandleFunc("/", homecontroller.Welcome)
+
 	log.Println("Starting server on :8080")
 	http.ListenAndServe(":8080", nil)
 }
